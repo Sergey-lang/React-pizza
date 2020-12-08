e9ef8bfa26b61b016324119c98a51263854af5f5
Sergey <sergei.kuharyonok@yandex.by>
2020-12-08 22:48:03 +0300

    install className library

---
+++ src/u3-components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export const Button = (props) => {
+
+   return (
+       <button className={classNames('button', {
+          'button--outline': props.outline,
+       })}>{props.children}</button>
+   )
+}
